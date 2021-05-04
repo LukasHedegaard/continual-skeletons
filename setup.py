79ede85185a2e5ea3b5500d9cef76de32ef47adf
LukasHedegaard <lh@eng.au.dk>
2021-05-04 09:52:50 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ def from_file(file_name: str = "requirements.txt", comment_char: str = "#"):
 
 
 setup(
-    name="st-gcn",
-    version="0.1.1",
+    name="cost-gcn",
+    version="0.1.2",
     description="Research repository for Continual Spatio-Temporal Graph Convolutional Neural Networks",
     long_description=long_description(),
     long_description_content_type="text/markdown",
