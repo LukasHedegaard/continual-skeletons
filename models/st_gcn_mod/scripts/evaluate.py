4aa44f6cc76d2feb97f7b047fd9c6ebe5173d3ab
LukasHedegaard <lh@eng.au.dk>
2021-05-31 08:36:22 +0000

    Add modified ST-GCN with no padding

---
+++ models/st_gcn_mod/scripts/evaluate.py
@@ -0,0 +1,59 @@
+import subprocess
+import os
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+ROOT_PATH = Path(os.getenv("ROOT_PATH", default=""))
+LOGS_PATH = Path(os.getenv("LOGS_PATH", default="logs"))
+DATASETS_PATH = Path(os.getenv("DATASETS_PATH", default="datasets"))
+
+GPUS = "0,"
+LOGGING_BACKEND = "wandb"
+DS_NAME = "ntu"
+DS_PATH = DATASETS_PATH / "ntu60"
+
+for subset, modality, pretrained_model in [
+    ("xview", "joint", "stgcn/nturgbd60_cv/ntu_cv_stgcn_joint-49-29400.pt"),
+    # ("xsub", "joint", "stgcn/nturgbd60_cs/ntu_cs_stgcn_joint-49-31300.pt"),
+]:
+    subprocess.call(
+        [
+            "python3",
+            "models/st_gcn_mod/st_gcn_mod.py",
+            "--id",
+            f"eval_{DS_NAME}_{subset}_{modality}",
+            "--gpus",
+            GPUS,
+            "--test",
+            "--profile_model",
+            "--batch_size",
+            "128",
+            "--num_workers",
+            "8",
+            "--dataset_normalization",
+            "0",
+            "--dataset_name",
+            DS_NAME,
+            "--dataset_classes",
+            str(DS_PATH / "classes.yaml"),
+            "--dataset_train_data",
+            str(DS_PATH / subset / f"train_data_{modality}.npy"),
+            "--dataset_val_data",
+            str(DS_PATH / subset / f"val_data_{modality}.npy"),
+            "--dataset_test_data",
+            str(DS_PATH / subset / f"val_data_{modality}.npy"),
+            "--dataset_train_labels",
+            str(DS_PATH / subset / "train_label.pkl"),
+            "--dataset_val_labels",
+            str(DS_PATH / subset / "val_label.pkl"),
+            "--dataset_test_labels",
+            str(DS_PATH / subset / "val_label.pkl"),
+            "--finetune_from_weights",
+            str(ROOT_PATH / "pretrained_models" / pretrained_model),
+            "--logging_backend",
+            "wandb",
+            # "--help",
+        ]
+    )
---
+++ models/st_gcn_mod/scripts/train.py
@@ -0,0 +1,66 @@
+import subprocess
+import os
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+ROOT_PATH = Path(os.getenv("ROOT_PATH", default=""))
+LOGS_PATH = Path(os.getenv("LOGS_PATH", default="logs"))
+DATASETS_PATH = Path(os.getenv("DATASETS_PATH", default="datasets"))
+
+DS_NAME = "ntu"
+DS_PATH = DATASETS_PATH / "ntu60"
+DS_SUBSET = "xview"
+MODALITY = "joint"
+
+
+subprocess.call(
+    [
+        "python3",
+        "models/cost_gcn/cost_gcn.py",
+        "--id",
+        "train",
+        "--gpus",
+        "4",
+        "--forward_mode",
+        "clip",
+        "--train",
+        "--test",
+        "--max_epochs",
+        "30",
+        "--optimization_metric",
+        "top1acc",
+        "--test",
+        "--batch_size",
+        "15",
+        "--num_workers",
+        "15",
+        "--learning_rate",
+        "0.2",
+        "--weight_decay",
+        "0.0001",
+        "--dataset_normalization",
+        "0",
+        "--dataset_name",
+        DS_NAME,
+        "--dataset_classes",
+        str(DS_PATH / "classes.yaml"),
+        "--dataset_train_data",
+        str(DS_PATH / DS_SUBSET / f"train_data_{MODALITY}.npy"),
+        "--dataset_val_data",
+        str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
+        "--dataset_test_data",
+        str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
+        "--dataset_train_labels",
+        str(DS_PATH / DS_SUBSET / "train_label.pkl"),
+        "--dataset_val_labels",
+        str(DS_PATH / DS_SUBSET / "val_label.pkl"),
+        "--dataset_test_labels",
+        str(DS_PATH / DS_SUBSET / "val_label.pkl"),
+        "--logging_backend",
+        "wandb",
+        "--distributed_backend",
+        "ddp",
+    ]
+)
