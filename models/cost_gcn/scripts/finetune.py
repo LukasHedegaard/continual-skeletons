6f1c67b0193f9cbced9745be31faa2a651196517
LukasHedegaard <lh@eng.au.dk>
2021-06-08 06:37:35 +0000

    Update cost_gcn ft script

--- models/cost_gcn/scripts/finetune.py
+++ models/cost_gcn/scripts/finetune.py
@@ -9,71 +9,68 @@ ROOT_PATH = Path(os.getenv("ROOT_PATH", default=""))
 LOGS_PATH = Path(os.getenv("LOGS_PATH", default="logs"))
 DATASETS_PATH = Path(os.getenv("DATASETS_PATH", default="datasets"))
 
-DS_NAME = "ntu"
+DS_NAME = "ntu60"
 DS_PATH = DATASETS_PATH / "ntu60"
 DS_SUBSET = "xview"
 MODALITY = "joint"
 
-# subprocess.call(
-#     [
-#         "python3",
-#         "models/cost_gcn/cost_gcn.py",
-#         "--id",
-#         "finetune_all_layers",
-#         "--gpus",
-#         "1",
-#         "--forward_mode",
-#         "clip",
-#         "--train",
-#         "--max_epochs",
-#         "10",
-#         "--optimization_metric",
-#         "top1acc",
-#         "--test",
-#         "--batch_size",
-#         "20",
-#         "--num_workers",
-#         "8",
-#         "--dataset_normalization",
-#         "0",
-#         "--dataset_name",
-#         DS_NAME,
-#         "--dataset_classes",
-#         str(DS_PATH / "classes.yaml"),
-#         "--dataset_train_data",
-#         str(DS_PATH / DS_SUBSET / f"train_data_{MODALITY}.npy"),
-#         "--dataset_val_data",
-#         str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
-#         "--dataset_test_data",
-#         str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
-#         "--dataset_train_labels",
-#         str(DS_PATH / DS_SUBSET / "train_label.pkl"),
-#         "--dataset_val_labels",
-#         str(DS_PATH / DS_SUBSET / "val_label.pkl"),
-#         "--dataset_test_labels",
-#         str(DS_PATH / DS_SUBSET / "val_label.pkl"),
-#         "--finetune_from_weights",
-#         str(
-#             ROOT_PATH
-#             / "pretrained_models"
-#             / "stgcn"
-#             / "nturgbd60_cv"
-#             / "ntu_cv_stgcn_joint-49-29400.pt"
-#         ),
-#         "--logging_backend",
-#         "wandb",
-#         "--pool_size",
-#         "56",
-#         "--unfreeze_from_epoch",
-#         "0",
-#         "--unfreeze_layers_initial",
-#         "-1",
-#         "--learning_rate",
-#         "0.0003125",  # Apply scaling rule: 0,001 / 64 * 20
-#         "--weight_decay",
-#         "0.0001",
-#     ]
-# )
+subprocess.call(
+    [
+        "python3",
+        "models/cost_gcn/cost_gcn.py",
+        "--id",
+        "finetune_all_layers",
+        "--gpus",
+        "3",
+        "--forward_mode",
+        "clip",
+        "--train",
+        "--max_epochs",
+        "5",
+        "--optimization_metric",
+        "top1acc",
+        "--test",
+        "--profile_model",
+        "--batch_size",
+        "20",
+        "--num_workers",
+        "8",
+        "--dataset_normalization",
+        "0",
+        "--dataset_name",
+        DS_NAME,
+        "--dataset_classes",
+        str(DS_PATH / "classes.yaml"),
+        "--dataset_train_data",
+        str(DS_PATH / DS_SUBSET / f"train_data_{MODALITY}.npy"),
+        "--dataset_val_data",
+        str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
+        "--dataset_test_data",
+        str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
+        "--dataset_train_labels",
+        str(DS_PATH / DS_SUBSET / "train_label.pkl"),
+        "--dataset_val_labels",
+        str(DS_PATH / DS_SUBSET / "val_label.pkl"),
+        "--dataset_test_labels",
+        str(DS_PATH / DS_SUBSET / "val_label.pkl"),
+        "--finetune_from_weights",
+        "models/st_gcn/weights/stgcn_ntu60_xview_joint.pt",
+        "--logging_backend",
+        "wandb",
+        "--pool_size",
+        "56",
+        "--unfreeze_from_epoch",
+        "0",
+        "--unfreeze_layers_initial",
+        "-1",
+        "--learning_rate",
+        "0.03125",  # Apply scaling rule: 0,1 / 64 * 20
+        "--weight_decay",
+        "0.0001",
+        "--distributed_backend",
+        "ddp",
+    ]
+)
 
 
 # subprocess.call(
@@ -139,65 +136,65 @@ MODALITY = "joint"
 #     ]
 # )
 
-subprocess.call(
-    [
-        "python3",
-        "models/cost_gcn/cost_gcn.py",
-        "--id",
-        "finetune_bn",
-        "--gpus",
-        "1",
-        "--forward_mode",
-        "clip",
-        "--train",
-        "--max_epochs",
-        "10",
-        "--optimization_metric",
-        "top1acc",
-        "--test",
-        "--batch_size",
-        "22",
-        "--num_workers",
-        "8",
-        "--dataset_normalization",
-        "0",
-        "--dataset_name",
-        DS_NAME,
-        "--dataset_classes",
-        str(DS_PATH / "classes.yaml"),
-        "--dataset_train_data",
-        str(DS_PATH / DS_SUBSET / f"train_data_{MODALITY}.npy"),
-        "--dataset_val_data",
-        str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
-        "--dataset_test_data",
-        str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
-        "--dataset_train_labels",
-        str(DS_PATH / DS_SUBSET / "train_label.pkl"),
-        "--dataset_val_labels",
-        str(DS_PATH / DS_SUBSET / "val_label.pkl"),
-        "--dataset_test_labels",
-        str(DS_PATH / DS_SUBSET / "val_label.pkl"),
-        "--finetune_from_weights",
-        str(
-            ROOT_PATH
-            / "pretrained_models"
-            / "stgcn"
-            / "nturgbd60_cv"
-            / "ntu_cv_stgcn_joint-49-29400.pt"
-        ),
-        "--logging_backend",
-        "wandb",
-        "--pool_size",
-        "56",
-        "--unfreeze_from_epoch",
-        "0",
-        "--unfreeze_layers_initial",
-        "-1",
-        "--unfreeze_layers_must_include",
-        "bn",
-        "--learning_rate",
-        "0.00034375",  # Apply scaling rule: 0,001 / 64 * 22
-        "--weight_decay",
-        "0.0001",
-    ]
-)
+# subprocess.call(
+#     [
+#         "python3",
+#         "models/cost_gcn/cost_gcn.py",
+#         "--id",
+#         "finetune_bn",
+#         "--gpus",
+#         "1",
+#         "--forward_mode",
+#         "clip",
+#         "--train",
+#         "--max_epochs",
+#         "10",
+#         "--optimization_metric",
+#         "top1acc",
+#         "--test",
+#         "--batch_size",
+#         "22",
+#         "--num_workers",
+#         "8",
+#         "--dataset_normalization",
+#         "0",
+#         "--dataset_name",
+#         DS_NAME,
+#         "--dataset_classes",
+#         str(DS_PATH / "classes.yaml"),
+#         "--dataset_train_data",
+#         str(DS_PATH / DS_SUBSET / f"train_data_{MODALITY}.npy"),
+#         "--dataset_val_data",
+#         str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
+#         "--dataset_test_data",
+#         str(DS_PATH / DS_SUBSET / f"val_data_{MODALITY}.npy"),
+#         "--dataset_train_labels",
+#         str(DS_PATH / DS_SUBSET / "train_label.pkl"),
+#         "--dataset_val_labels",
+#         str(DS_PATH / DS_SUBSET / "val_label.pkl"),
+#         "--dataset_test_labels",
+#         str(DS_PATH / DS_SUBSET / "val_label.pkl"),
+#         "--finetune_from_weights",
+#         str(
+#             ROOT_PATH
+#             / "pretrained_models"
+#             / "stgcn"
+#             / "nturgbd60_cv"
+#             / "ntu_cv_stgcn_joint-49-29400.pt"
+#         ),
+#         "--logging_backend",
+#         "wandb",
+#         "--pool_size",
+#         "56",
+#         "--unfreeze_from_epoch",
+#         "0",
+#         "--unfreeze_layers_initial",
+#         "-1",
+#         "--unfreeze_layers_must_include",
+#         "bn",
+#         "--learning_rate",
+#         "0.00034375",  # Apply scaling rule: 0,001 / 64 * 22
+#         "--weight_decay",
+#         "0.0001",
+#     ]
+# )
