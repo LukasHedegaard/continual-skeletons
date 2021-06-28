988031d5055c190bcaeb6d7773c92de9b5430b49
LukasHedegaard <lh@eng.au.dk>
2021-06-28 06:31:08 +0000

    Update a_gcn_mod with AdaptiveGraphConvolutionMod

--- models/a_gcn_mod/a_gcn_mod.py
+++ models/a_gcn_mod/a_gcn_mod.py
@@ -1,11 +1,67 @@
 import ride  # isort:skip
 
+import numpy as np
+import torch
 import torch.nn as nn
 
 from datasets import datasets
-from models.a_gcn.a_gcn import AdaptiveGraphConvolution
 from models.base import StGcnBlock
 from models.utils import init_weights
+
+
+class AdaptiveGraphConvolutionMod(nn.Module):
+    def __init__(self, in_channels, out_channels, A, bn_momentum=0.1, coff_embedding=4):
+        super(AdaptiveGraphConvolutionMod, self).__init__()
+        self.inter_c = out_channels // coff_embedding
+        self.graph_attn = nn.Parameter(torch.from_numpy(A.astype(np.float32)))
+        nn.init.constant_(self.graph_attn, 1)
+        self.A = nn.Parameter(
+            torch.from_numpy(A.astype(np.float32)), requires_grad=False
+        )
+        self.num_subset = 3
+        self.g_conv = nn.ModuleList()
+        self.a_conv = nn.ModuleList()
+        self.b_conv = nn.ModuleList()
+        for i in range(self.num_subset):
+            self.g_conv.append(nn.Conv2d(in_channels, out_channels, 1))
+            self.a_conv.append(nn.Conv2d(in_channels, self.inter_c, 1))
+            self.b_conv.append(nn.Conv2d(in_channels, self.inter_c, 1))
+            init_weights(self.g_conv[i], bs=self.num_subset)
+            init_weights(self.a_conv[i])
+            init_weights(self.b_conv[i])
+
+        if in_channels != out_channels:
+            self.gcn_residual = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1),
+                nn.BatchNorm2d(out_channels, momentum=bn_momentum),
+            )
+            init_weights(self.gcn_residual[0], bs=1)
+            init_weights(self.gcn_residual[1], bs=1)
+        else:
+            self.gcn_residual = lambda x: x
+
+        self.bn = nn.BatchNorm2d(out_channels, momentum=bn_momentum)
+        init_weights(self.bn, bs=1e-6)
+        self.relu = nn.ReLU()
+        self.soft = nn.Softmax(-2)
+
+    def forward(self, x):
+        N, C, T, V = x.size()
+        A = self.A + self.graph_attn
+        hidden = None
+        for i in range(self.num_subset):
+            A1 = self.a_conv[i](x).permute(0, 3, 1, 2).contiguous()
+            A2 = self.b_conv[i](x)
+            # Modified attention within timestep
+            A1 = self.soft(
+                torch.einsum("nvct,nctw->nvwt", A1, A2) / self.inter_c
+            )  # N V V T
+            A1 = A1 + A[i].unsqueeze(0).unsqueeze(-1)
+            z = self.g_conv[i](torch.einsum("nctv,nvwt->nctv", x, A1))
+            hidden = z + hidden if hidden is not None else z
+        hidden = self.bn(hidden)
+        hidden += self.gcn_residual(x)
+        return self.relu(hidden)
 
 
 class AGcnMod(
@@ -25,7 +81,7 @@ class AGcnMod(
         # Define layers
         self.data_bn = nn.BatchNorm1d(num_skeletons * num_channels * num_vertices)
 
-        GraphConv = AdaptiveGraphConvolution
+        GraphConv = AdaptiveGraphConvolutionMod
         # fmt: off
         self.layers = nn.ModuleDict(
             {
